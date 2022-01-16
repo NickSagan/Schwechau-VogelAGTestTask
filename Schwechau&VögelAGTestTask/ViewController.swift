c5ce1cd2e3dbe8afab35ac0296afa590a740ff6e
NickSagan <9952728@gmail.com>
2022-01-16 15:22:39 +0300

    Initial Commit

---
+++ Schwechau&VögelAGTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Schwechau&VögelAGTestTask
+//
+//  Created by Nick Sagan on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
