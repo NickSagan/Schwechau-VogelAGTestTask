dcd29b93d44ac90b9e9cd40c0fd71493817e2878
NickSagan <9952728@gmail.com>
2022-01-16 17:55:27 +0300

    start creating struct BookSearchManager

---
+++ Schwechau&VögelAGTestTask/BookSearchManager.swift
@@ -0,0 +1,25 @@
+//
+//  BookSearchManager.swift
+//  Schwechau&VögelAGTestTask
+//
+//  Created by Nick Sagan on 16.01.2022.
+//
+
+import Foundation
+
+struct BookSearchManager {
+    
+    let googleapisUrl = "https://www.googleapis.com/books/v1/volumes?q="
+    
+    func fetchBooks() {
+        
+    }
+    
+    func performRequest() {
+        
+    }
+    
+    func parseJSON() {
+        
+    }
+}
