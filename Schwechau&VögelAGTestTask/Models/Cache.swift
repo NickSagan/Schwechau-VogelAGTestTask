a8632ede70e770f35913d41438c6f25fa24417d6
NickSagan <9952728@gmail.com>
2022-01-17 11:03:45 +0300

    add Cache struct

---
+++ Schwechau&VögelAGTestTask/Models/Cache.swift
@@ -0,0 +1,12 @@
+//
+//  Cache.swift
+//  Schwechau&VögelAGTestTask
+//
+//  Created by Nick Sagan on 17.01.2022.
+//
+
+import UIKit
+
+struct Cache {
+    static var cache = NSCache <NSString, UIImage> ()
+}
